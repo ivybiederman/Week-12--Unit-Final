ef082e0ebf56a7876f7589b0cc999d0cb7ba01be
ivybiederman <ivybiederman@gmail.com>
2024-01-13 14:04:28 -0700

    Add comments to JavaScript code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Class representing a Blog
 class Blog {
     constructor(title, content) {
         this.title = title;
@@ -5,21 +6,27 @@ class Blog {
     }
 }
 
+// Service class for handling CRUD operations on blogs
 class BlogService {
+    // API endpoint URL for blogs
     static url = 'https://65a0b515600f49256fb02c99.mockapi.io/blogs';
 
+    // Retrieve all blogs from the API
     static getAllBlogs() {
         return $.get(this.url);
     }
 
+    // Retrieve a specific blog by ID from the API
     static getBlog(id) {
         return $.get(`${this.url}/${id}`);
     }
 
+    // Create a new blog entry on the API
     static createBlog(blog) {
         return $.post(this.url, blog);
     }
 
+    // Update an existing blog entry on the API
     static updateBlog(blog) {
         return $.ajax({
             url: `${this.url}/${blog.id}`,
@@ -30,6 +37,7 @@ class BlogService {
         });
     }
 
+    // Delete a blog entry by ID from the API
     static deleteBlog(id) {
         return $.ajax({
             url: `${this.url}/${id}`,
@@ -38,13 +46,17 @@ class BlogService {
     }
 }
 
+// Class managing the DOM interactions for blogs
 class DOMManager {
+    // Array to store retrieved blogs
     static blogs;
 
+    // Retrieve all blogs and render them in the DOM
     static getAllBlogs() {
         BlogService.getAllBlogs().then(blogs => this.render(blogs));
     }
 
+    // Create a new blog and display it in the DOM
     static createAndDisplayBlog() {
         const title = $('#new-blog-title').val();
         const content = $('#new-blog-content').val();
@@ -60,6 +72,7 @@ class DOMManager {
             });
     }
 
+    // Delete a blog entry by ID and refresh the list in the DOM
     static deleteBlog(id) {
         BlogService.deleteBlog(id)
             .then(() => {
@@ -71,10 +84,12 @@ class DOMManager {
             });
     }
 
+    // Render the blogs in the DOM
     static render(blogs) {
         this.blogs = blogs;
         $('#app').empty();
         for (let blog of blogs) {
+            // Display each blog entry in a card format
             $('#app').prepend(
                 `<div id="${blog.id}" class="card">
                     <div class="card-header">
@@ -90,9 +105,12 @@ class DOMManager {
     }
 }
 
+// Execute when the document is ready
 $(document).ready(function () {
+    // Initialize the DOMManager and retrieve all blogs
     DOMManager.getAllBlogs();
 
+    // Event listener for creating a new blog
     $('#create-new-blog').click(function () {
         DOMManager.createAndDisplayBlog();
     });
